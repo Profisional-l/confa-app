5ce7cbdaef1d192b8feb07430a47b8bb18dff7f4
Jake Sooly <96531067+Profisional-l@users.noreply.github.com>
2024-09-26 22:34:45 +0300

    roje23f

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-
 import Navbar from './Navbar.jsx'
 import Announcements from './Announcements.jsx'
 
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return(
+        <head>
+            <script src="https://telegram.org/js/telegram-web-app.js"></script>
+        </head>
+    );
+}
+
+export default Header
